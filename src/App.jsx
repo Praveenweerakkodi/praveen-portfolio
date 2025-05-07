397f87c53d63458fd15bda7f597e656ccaaacea0
Praveen Weerakkodi <praveenweerakkodi@gmail.com>
2025-05-07 22:41:42 +0530

    fixed bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import React, { useState } from 'react';
 import "./index.css";
 import Home from "./Pages/Home";
@@ -11,6 +11,26 @@ import ProjectDetails from "./components/ProjectDetail";
 import WelcomeScreen from "./Pages/WelcomeScreen";
 import { AnimatePresence } from 'framer-motion';
 
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
+    <AnimatedBackground />
+    {children}
+    <footer>
+      <center>
+        <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
+        <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
+          © 2025{" "}
+          <a href="https://flowbite.com/" className="hover:underline">
+            PraveenNW™
+          </a>
+          . All Rights Reserved.
+        </span>
+      </center>
+    </footer>
+  </>
+);
+
 const LandingPage = ({ showWelcome, setShowWelcome }) => {
   return (
     <>
@@ -21,57 +41,39 @@ const LandingPage = ({ showWelcome, setShowWelcome }) => {
       </AnimatePresence>
 
       {!showWelcome && (
-        <>
-          <Navbar />
-          <AnimatedBackground />
+        <Layout>
           <Home />
           <About />
           <Portofolio />
           <ContactPage />
-          <footer>
-            <center>
-              <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
-              <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-                © 2025{" "}
-                <a href="https://flowbite.com/" className="hover:underline">
-                  PraveenNW™
-                </a>
-                . All Rights Reserved.
-              </span>
-            </center>
-          </footer>
-        </>
+        </Layout>
       )}
     </>
   );
 };
 
 const ProjectPageLayout = () => (
-  <>
+  <Layout>
     <ProjectDetails />
-    <footer>
-      <center>
-        <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
-        <span className="block text-sm pb-4 text-gray-500 text-center dark:text-gray-400">
-          © 2023{" "}
-          <a href="https://flowbite.com/" className="hover:underline">
-          PraveenNW™
-          </a>
-          . All Rights Reserved.
-        </span>
-      </center>
-    </footer>
-  </>
+  </Layout>
 );
 
 function App() {
   const [showWelcome, setShowWelcome] = useState(true);
 
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/praveen-portfolio">
       <Routes>
         <Route path="/" element={<LandingPage showWelcome={showWelcome} setShowWelcome={setShowWelcome} />} />
         <Route path="/project/:id" element={<ProjectPageLayout />} />
+        
+        {/* Add these if you want separate pages */}
+        <Route path="/about" element={<Layout><About /></Layout>} />
+        <Route path="/portfolio" element={<Layout><Portofolio /></Layout>} />
+        <Route path="/contact" element={<Layout><ContactPage /></Layout>} />
+        
+        {/* Catch-all route */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
